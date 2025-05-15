dc1d44bf4e8a4e37c1781b40972e1fc75d5af016
vinay.b <vinay.b@darwinbox.io>
2025-05-15 13:27:21 +0530

    [116] HostBinding & Host Listener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-control',
   standalone: true,
@@ -8,8 +8,19 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)' : 'onClick()'
   },
 })
 export class ControlComponent {
+  //@HostBinding('class') className = 'control';
+
+  // @HostListener('click') onClick() {
+  //   console.log("cklicked");
+  // }
+
   @Input({ required: true }) label!: string;
+
+  onClick() {
+    console.log("cklicked");
+  }
 }
